d3283c8b185e58b714d060de3559b040f912b96c
MD. Mahmudul Hasan <61178414+mahmudulri@users.noreply.github.com>
2022-09-21 10:01:21 +0600

    tested

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,6 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
         ),
         getPages: getPages,
-        home: LogIn_Screen());
+        home: SignUpScreen());
   }
 }
